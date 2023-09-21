f21a658710e315ecc7be2815c9748b8cad86f375
Tiannn <septian.aldi166@gmail.com>
2023-09-21 22:51:24 +0700

    Bismillah jadi

--- lib/ui/tampil_data.dart
+++ lib/ui/tampil_data.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:tugas_2/ui/form_data.dart';
 
 class TampilData extends StatelessWidget {
-  final String data; //contoh data yang di-passing
-  
-  const TampilData({Key? key, required this.data}) : super(key: key);
+  final String nama;
+  final String nim;
+  final int tahunLahir;
+
+  const TampilData(
+      {Key? key,
+      required this.nama,
+      required this.nim,
+      required this.tahunLahir})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Perkenalan"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Perkenalan"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Text(
+                  "Nama saya $nama , NIM $nim, dan umur saya adalah ${hitungUmur(tahunLahir)} tahun"),
+            ],
+          ),
+        ));
   }
 }
